9ce65ac867401ef4493851006a8fe731ed6973dc
cyberspacex <126796642+oluwatay123@users.noreply.github.com>
2024-05-09 17:55:05 +0100

    Update gemini.controller.js

--- src/controllers/gemini.controller.js
+++ src/controllers/gemini.controller.js
@@ -2,7 +2,7 @@ const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
 require('dotenv').config();
 const { GoogleGenerativeAI } = require('@google/generative-ai')
-const generativeAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY)
+const generativeAI = new GoogleGenerativeAI("AIzaSyBeVUNfTWGe3SEkVAorGXVpFmYifbj4NUs")
 
 
 exports.sendResponse = catchAsync(async (req, res, next) => {
